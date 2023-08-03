40ecdf32686381ae04e80e208e90c2626a668e3e
Tabares1 <alejotabares1902@gmail.com>
2023-08-02 23:26:54 -0500

    ejercicios del 1 al 7

---
+++ app.py
@@ -0,0 +1,20 @@
+from ejercicios import calcular_area_circulo
+calcular_area_circulo.calcular_area_circulo()
+
+from ejercicios import convertir_grados
+convertir_grados.convertir_grados()
+
+from ejercicios import imprimir_nombre_edad
+imprimir_nombre_edad.imprimir_nombre_edad()
+
+from ejercicios import lista_num_aleatorios
+lista_num_aleatorios.lista_num_aleatorios()
+
+from ejercicios import num_mayor_menor
+num_mayor_menor.num_mayor_menor()
+
+from ejercicios import num_par_impa
+num_par_impa.num_par_impar()
+
+from ejercicios import suma_num_lista
+suma_num_lista.suma_num_lista()
